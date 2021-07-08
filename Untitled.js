d9a5bc0c25fb14aa3a4412e8d09cd41c6252186e
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2021-07-08 14:21:27 +0530

    Untitled.js

--- Untitled.js
+++ Untitled.js
@@ -1,7 +1,7 @@
 var myVar;
 
 function myFunction(){
-    myVar = setTimeout(showPage, 3500);
+    myVar = setTimeout(showPage, 1500);
 }
 function showPage(){
     document.getElementById("loader-wrapper") .style.display = "none";
