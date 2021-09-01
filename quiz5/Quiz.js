dee7677855fb85c1850da4650544c79b8eb0693a
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2021-09-01 20:01:30 +0530

    Create Quiz.js

---
+++ quiz5/Quiz.js
@@ -0,0 +1,44 @@
+var myVar;
+
+function myFunction(){
+    myVar = setTimeout(showPage, 3500);
+    document.getElementById("circle1").style.display = "block";
+    document.getElementById("circle2").style.display = "none";
+    document.getElementById("text").style.color = "white";
+    document.getElementById("text1").style.color = "white";
+    document.getElementById("wassap1").style.display = "none";
+    document.getElementById("post1").style.display = "none";
+    document.getElementById("tweet1").style.display = "none";
+}
+function showPage(){
+    document.getElementById("loader-wrapper") .style.display = "none";
+    document.getElementById("content") .style.display = "block";
+}
+function white(){
+    document.body.style.background = "white";
+    document.getElementById("refresh").style.color = "black";
+    document.getElementById("text").style.color = "black";
+    document.getElementById("text1").style.color = "black";
+    document.getElementById("circle2").style.display = "block";
+    document.getElementById("circle1").style.display = "none";
+    document.getElementById("wassap").style.display = "none";
+    document.getElementById("post").style.display = "none";
+    document.getElementById("tweet").style.display = "none";
+    document.getElementById("wassap1").style.display = "block";
+    document.getElementById("post1").style.display = "block";
+    document.getElementById("tweet1").style.display = "block";
+}
+function black(){
+    document.body.style.background = "black";
+    document.getElementById("refresh").style.color = "white";
+    document.getElementById("text").style.color = "white";
+    document.getElementById("text1").style.color = "white";
+    document.getElementById("circle1").style.display = "block";
+    document.getElementById("circle2").style.display = "none";
+    document.getElementById("wassap1").style.display = "none";
+    document.getElementById("post1").style.display = "none";
+    document.getElementById("tweet1").style.display = "none";
+    document.getElementById("wassap").style.display = "block";
+    document.getElementById("post").style.display = "block";
+    document.getElementById("tweet").style.display = "block";
+}
