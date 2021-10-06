42701fbab006790697d32c58c83149e3f4f1c64b
Devraj Therani <83823130+devrajtherani@users.noreply.github.com>
2021-10-06 20:51:54 +0530

    Update Untitled.js

--- quiz1/Untitled.js
+++ quiz1/Untitled.js
@@ -2,8 +2,6 @@ var myVar;
 
 function myFunction(){
     myVar = setTimeout(showPage, 1500);
-    document.getElementById("arrow1").style.color = "white";
-    document.getElementById("arrow2").style.color = "white";
 }
 function showPage(){
     document.getElementById("loader-wrapper") .style.display = "none";
